bc14a636035f675b83d4efb62ebbcc7e9f73e1bf
Évelim Dornelles <dorrrnelles@gmail.com>
2024-06-13 17:21:33 -0300

    Alterações no layout e imagens

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ window.onscroll = () => {
             });
         };
     });
+
     /*SCTIKY NAVBAR */
 
     let header = document.querySelector('header');
@@ -52,3 +53,4 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
